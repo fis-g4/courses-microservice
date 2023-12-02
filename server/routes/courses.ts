0cd223d6e36ea477df3b6aeeaa62c697ae1db8ad
MatthewBL <xamtehwt@gmail.com>
2023-12-02 15:39:17 +0100

    FEAT: Filters for language and categories added

--- server/routes/courses.ts
+++ server/routes/courses.ts
@@ -4,63 +4,99 @@ import { Course } from '../db/models/course';
 const router = express.Router()
 
 router.get('/', async (req: Request, res: Response) => {
-    const courses = await Course.find({})
+    try {
+      let filters: { [key: string]: any } = {};
 
-    return res.status(200).json(courses)
+      // Loop through query parameters
+      Object.keys(req.query).forEach((key) => {
+          // If the attribute is a list, handle it with $in
+          if (key === 'categories' || key === 'otherListAttribute') {
+            // @ts-ignore
+            filters[key] = { $all: Array.isArray(req.query[key]) ? req.query[key].split(" ") : req.query[key].split(" ") };
+          } else {
+            // If it's a single value attribute, directly assign the value
+            filters[key] = req.query[key];
+          }
+      });
+
+      const courses = await Course.find(filters);
+
+      return res.status(200).json(courses);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
 })
 
 router.get('/:courseId', async (req: Request, res: Response) => {
-    const courseId = req.params.courseId;
-    const course = await Course.findById(courseId);
-
-    return res.status(200).json(course)
+    try {
+      const courseId = req.params.courseId;
+      const course = await Course.findById(courseId);
+      if (course)
+        return res.status(200).json(course)
+      else
+      return res.status(404).json({ error: 'Course not found' });
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
 })
 
 router.post('/', async (req: Request, res: Response) => {
-  const { name, description, price, categories, language, creator }: CourseFormInputs = req.body
+  try {
+    const { name, description, price, categories, language, creator }: CourseFormInputs = req.body
 
-  const course = Course.build({
-    name: name, 
-    description: description, 
-    price: price,
-    categories: categories,
-    language: language,
-    creator: creator,
-  });
+    const course = Course.build({
+      name: name, 
+      description: description, 
+      price: price,
+      categories: categories,
+      language: language,
+      creator: creator,
+    });
 
-  await course.save();
+    await course.save();
 
-  return res.status(201).send('Course created!')
+    return res.status(201).send('Course created!')
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 })
 
 router.put('/:courseId', async (req: Request, res: Response) => {
-  const { name, description, price, categories, language, creator }: CourseFormInputs = req.body
-  const courseId = req.params.courseId;
-
-  var course = await Course.findById(courseId);
-  if (course) {
-    course.name = name;
-    course.description = description;
-    course.price = price;
-    course.categories = categories;
-    course.language = language;
-    course.creator = creator;
-
-    await course.save();
+  try {
+    const { name, description, price, categories, language, creator }: CourseFormInputs = req.body
+    const courseId = req.params.courseId;
   
-    return res.status(201).json(course)
-  }
-  else {
-    return res.status(404).json(course)
+    var course = await Course.findById(courseId);
+    if (course) {
+      course.name = name;
+      course.description = description;
+      course.price = price;
+      course.categories = categories;
+      course.language = language;
+      course.creator = creator;
+  
+      await course.save();
+    
+      return res.status(201).json(course)
+    }
+    else {
+      return res.status(404).json(course)
+    }
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
   }
 })
 
 router.delete('/:courseId', async (req: Request, res: Response) => {
-  const courseId = req.params.courseId;
-
-  await Course.deleteOne({ _id : courseId })
+  try {
+    const courseId = req.params.courseId;
   
-  return res.status(200).send("Course deleted!")
+    await Course.deleteOne({ _id : courseId })
+    
+    return res.status(200).send("Course deleted!")
+  } catch (error) {
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
 })
 
 export default router
