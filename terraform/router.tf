f37125f3093ef98c9a831a9bf69eaa6bbddfbcb3
MatthewBL <xamtehwt@gmail.com>
2024-01-15 15:31:54 +0100

    FIX: router.tf updated

--- terraform/router.tf
+++ terraform/router.tf
@@ -17,7 +17,7 @@ resource "google_compute_subnetwork" "courses-service-subnetwork" {
 
 resource "google_compute_router_nat" "fis_g4_cloud_nat" {
   name                               = var.cloud_nat_name
-  router                             = google_compute_router.courses-service-router.name
+  router                             = google_compute_router.fis_g4_router.name
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
