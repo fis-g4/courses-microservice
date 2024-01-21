54087c22b3854c69d45fa466fce9431fba83cf97
MatthewBL <xamtehwt@gmail.com>
2024-01-21 13:27:45 +0100

    FIX: New deployment

--- terraform/group.tf
+++ terraform/group.tf
@@ -1,4 +1,4 @@
-resource "google_compute_http_health_check" "courses_service_health_check" {
+resource "google_compute_http_health_check" "courses-service-health-check" {
   name         = var.health_check_name
   request_path = var.health_check_path
 
@@ -6,7 +6,7 @@ resource "google_compute_http_health_check" "courses_service_health_check" {
   check_interval_sec = 5
 }
 
-resource "google_compute_instance_group" "courses_service_group" {
+resource "google_compute_instance_group" "courses-service-group" {
   name        = var.group_name
   description = var.group_description
 
@@ -27,17 +27,17 @@ resource "google_compute_instance_group" "courses_service_group" {
   zone = var.zone
 }
 
-resource "google_compute_backend_service" "courses_service_backend" {
+resource "google_compute_backend_service" "courses-service-backend" {
   name      = var.backend_service_name
   port_name = "http"
   protocol  = "HTTP"
   load_balancing_scheme = "EXTERNAL_MANAGED"
 
   backend {
-    group = google_compute_instance_group.courses_service_group.id
+    group = google_compute_instance_group.courses-service-group.id
   }
 
   health_checks = [
-    google_compute_http_health_check.courses_service_health_check.id,
+    google_compute_http_health_check.courses-service-health-check.id,
   ]
 }
