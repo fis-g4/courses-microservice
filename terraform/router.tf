af121531e82d09ed02abdd8e3bf4bcdd965adb00
MatthewBL <xamtehwt@gmail.com>
2024-01-21 13:15:31 +0100

    FIX: New deployment

--- terraform/router.tf
+++ terraform/router.tf
@@ -12,12 +12,12 @@ resource "google_compute_subnetwork" "courses_service_subnetwork" {
   name          = "courses-service-subnetwork"
   ip_cidr_range = "10.0.60.0/24"
   region        = var.region
-  network       = "fis_g4_network_cd"
+  network       = "fis-g4-network-cd"
 }
 
 resource "google_compute_router_nat" "courses_service_cloud_nat" {
   name                               = var.cloud_nat_name
-  router                             = google_compute_router.fis_g4_router.name
+  router                             = google_compute_router.courses_service_router.name
   region                             = var.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
