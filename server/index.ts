f54cb61e223b612b2b7e969124d5a8a7220d12b6
davbarsal1 <72515663+davbarsal1@users.noreply.github.com>
2024-01-24 11:44:47 +0100

    FEAT: Documentación

--- server/index.ts
+++ server/index.ts
@@ -1,21 +1,139 @@
-import express, { Express, Request, Response } from 'express'
+import express, { Express, NextFunction, Request, Response } from 'express'
 import './loadEnvironment'
-import { receiveMessages } from './rabbitmq/operations'
 import cors from 'cors'
 import courses from './routes/courses'
 import reviews from './routes/reviews'
 import './db/conn'
 import { generateToken, verifyToken } from './utils/jwtUtils'
-
+import yaml from 'yaml'
+import fs from 'fs';
+import { receiveMessages } from './rabbitmq/operations'
+import swaggerjsdoc from 'swagger-jsdoc'
+import swaggerui from 'swagger-ui-express'
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
 
+const swaggerJsDoc = swaggerjsdoc
+const swaggerUI = swaggerui
+const swaggerUICourse = swaggerui
+
+const swaggerOptionsReviews = {
+    definition: {
+        openapi: '3.1.0',
+        info: {
+            version: '1.0.0',
+            title: 'Reviews/Courses Microservice API',
+            description:
+                'API for the reviews/courses microservice of the FIS-G4 project.',
+            contact: {
+                name: 'David Barragán y Mathew Byte',
+                email: '',
+                url: 'https://github.com/fis-g4/courses-microservice',
+            },
+            license: {
+                name: 'MIT',
+                url: 'https://opensource.org/licenses/MIT',
+            },
+        },
+        servers: [
+            {
+                url:
+                    process.env.BASE_URL ?? 'http://localhost:8000/v1/reviews',
+            },
+            {
+                url:
+                    process.env.BASE_URL ?? 'http://localhost:8000/v1/courses',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    apis: ['./routes/reviews.ts', './routes/courses.ts'],
+}
+/*
+const swaggerOptionsCourses = {
+    definition: {
+        openapi: '3.1.0',
+        info: {
+            version: '1.0.0',
+            title: 'Reviews/Courses Microservice API',
+            description:
+                'API for the reviews/courses microservice of the FIS-G4 project.',
+            contact: {
+                name: 'Mathew Byte',
+                email: '',
+                url: 'https://github.com/fis-g4/courses-microservice',
+            },
+            license: {
+                name: 'MIT',
+                url: 'https://opensource.org/licenses/MIT',
+            },
+        },
+        servers: [
+            {
+                url:
+                    process.env.BASE_URL ?? 'http://localhost:8000/v1/reviews',
+            },
+            {
+                url:
+                    process.env.BASE_URL ?? 'http://localhost:8000/v1/courses',
+            },
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT',
+                },
+            },
+        },
+        security: [
+            {
+                bearerAuth: [],
+            },
+        ],
+    },
+    apis: ['./routes/courses.ts'],
+}
+
+*/
+const swaggerDocsReviews = swaggerJsDoc(swaggerOptionsReviews);
+const yamlStringReviews: string = yaml.stringify(swaggerDocsReviews, {});
+//const swaggerDocsCourses = swaggerJsDoc(swaggerOptionsCourses);
+//const yamlStringCourses: string = yaml.stringify(swaggerDocsCourses, {});
+fs.writeFileSync('./docs/swaggerReviews.yaml', yamlStringReviews);
+//fs.writeFileSync('./docs/swaggerCourses.yaml', yamlStringCourses);
+app.use(
+    '/v1/courses/docs/',
+    swaggerUI.serve,
+    swaggerUI.setup(swaggerDocsReviews, { explorer: true })
+);
+/*
+app.use(
+    '/v1/courses/docs/',
+    swaggerUICourse.serve,
+    swaggerUICourse.setup(swaggerDocsCourses, { explorer: true })
+);
+*/
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World From the Courses Service!')
 })
-
+ 
 const URLS_ALLOWED_WITHOUT_TOKEN = ['/v1/courses/check']
 
 app.use((req, res, next) => {
@@ -48,11 +166,11 @@ app.use((req, res, next) => {
 const port = process.env.PORT ?? 8000
 
 const MICROSERVICE_QUEUE = 'courses_microservice'
-
 app.use('/v1/courses', courses)
 app.use('/v1/reviews', reviews)
 
 receiveMessages(MICROSERVICE_QUEUE)
+
 
 app.listen(port, () => {
     console.info(`Courses microservice listening on port ${port}`)
--- server/routes/courses.ts
+++ server/routes/courses.ts
@@ -11,24 +11,78 @@ import { MaterializedView } from '../db/models/materializedView';
 
 const router = express.Router()
 
-router.get('/check', async (req: Request, res: Response) => {
-  return res
-      .status(200)
-      .json({ message: 'The courses service is working properly!' })
-})
+/**
+ * @swagger
+ * tags:
+ *   name: Courses
+ *   description: API para gestionar cursos.
+ */
 
+/**
+ * @swagger
+ * definitions:
+ *   CourseFormInputs:
+ *     type: object
+ *     properties:
+ *       name:
+ *         type: string
+ *       description:
+ *         type: string
+ *       price:
+ *         type: number
+ *       categories:
+ *         type: array
+ *         items:
+ *           type: string
+ *       language:
+ *         type: string
+ */
+
+/**
+ * @swagger
+ * /courses/check:
+ *   get:
+ *     summary: Verifica que el servicio de cursos esté funcionando correctamente.
+ *     tags: [Courses]
+ *     responses:
+ *       200:
+ *         description: El servicio de cursos está funcionando correctamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               message: 'The courses service is working properly!'
+ *       500:
+ *         description: Error interno del servidor.
+ */
+router.get('/check', async (req: Request, res: Response) => {
+  return res.status(200).json({ message: 'The courses service is working properly!' });
+});
+
+/**
+ * @swagger
+ * /courses/categories:
+ *   get:
+ *     summary: Obtiene recuentos de categorías para los cursos.
+ *     tags: [Courses]
+ *     responses:
+ *       200:
+ *         description: Recuentos de categorías recuperados exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               category1: 5
+ *               category2: 10
+ *               category3: 7
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/categories', async (req: Request, res: Response) => {
   try {
     const courses = await Course.find({});
-
-    // Create an empty dictionary to store category counts
     const categoryCounts: { [key: string]: number } = {};
 
-    // Iterate through each course
     courses.forEach((course) => {
-      // Iterate through each category in the course
       course.categories.forEach((category) => {
-        // Update the count in the dictionary
         categoryCounts[category] = (categoryCounts[category] || 0) + 1;
       });
     });
@@ -37,33 +91,84 @@ router.get('/categories', async (req: Request, res: Response) => {
   } catch (error) {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
 
+/**
+ * @swagger
+ * /courses/best:
+ *   get:
+ *     summary: Obtiene los mejores cursos ordenados por puntuación.
+ *     tags: [Courses]
+ *     responses:
+ *       200:
+ *         description: Mejores cursos recuperados exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - name: 'Curso 1'
+ *                 description: 'Descripción del curso 1'
+ *                 price: 19.99
+ *                 categories: ['Categoria1', 'Categoria2']
+ *                 language: 'Español'
+ *                 creator: 'Usuario1'
+ *                 score: 4.5
+ *               - name: 'Curso 2'
+ *                 description: 'Descripción del curso 2'
+ *                 price: 29.99
+ *                 categories: ['Categoria3', 'Categoria4']
+ *                 language: 'Inglés'
+ *                 creator: 'Usuario2'
+ *                 score: 4.8
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/best', async (req: Request, res: Response) => {
   try {
     const courses = await Course.find().sort({ score: -1 }).limit(6);
 
-    const modifiedCourses = await Promise.all(courses.map(async course => {
-      const user = await MaterializedView.findOne({ username: course.creator });
-      if (user) {
-        return {
-          ...course.toObject(), // Convert Mongoose document to plain JavaScript object
-          creator: user.firstName + " " + user.lastName // Change 'newUsername' to the desired value
-        };
-      }
-      else {
-        return {
-          course
-        };
-      }
-    }))
+    const modifiedCourses = await Promise.all(
+      courses.map(async (course) => {
+        const user = await MaterializedView.findOne({ username: course.creator });
+        if (user) {
+          return {
+            ...course.toObject(),
+            creator: user.firstName + ' ' + user.lastName,
+          };
+        } else {
+          return {
+            course,
+          };
+        }
+      })
+    );
 
     return res.status(200).json(modifiedCourses);
   } catch (error) {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
-})
+});
 
+/**
+ * @swagger
+ * /courses/new:
+ *   post:
+ *     summary: Crea un nuevo curso.
+ *     tags: [Courses]
+ *     requestBody:
+ *       description: Datos del nuevo curso.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/CourseFormInputs'
+ *     responses:
+ *       201:
+ *         description: Curso creado exitosamente.
+ *       400:
+ *         description: Error de validación al guardar.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.post('/new', async (req: Request, res: Response) => {
   try {
     let decodedToken: IUser = await getPayloadFromToken(
@@ -123,7 +228,35 @@ router.post('/new', async (req: Request, res: Response) => {
     }
   }
 })
-
+/**
+ * @swagger
+ * /courses/list:
+ *   get:
+ *     summary: Obtiene la lista de cursos según los filtros proporcionados.
+ *     tags: [Courses]
+ *     responses:
+ *       200:
+ *         description: Lista de cursos recuperada exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - name: 'Curso 1'
+ *                 description: 'Descripción del curso 1'
+ *                 price: 19.99
+ *                 categories: ['Categoria1', 'Categoria2']
+ *                 language: 'Español'
+ *                 creator: 'Usuario1'
+ *                 score: 4.5
+ *               - name: 'Curso 2'
+ *                 description: 'Descripción del curso 2'
+ *                 price: 29.99
+ *                 categories: ['Categoria3', 'Categoria4']
+ *                 language: 'Inglés'
+ *                 creator: 'Usuario2'
+ *                 score: 4.8
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/list', async (req: Request, res: Response) => {
     try {
       let decodedToken: IUser = await getPayloadFromToken(
@@ -182,7 +315,44 @@ router.get('/list', async (req: Request, res: Response) => {
       return res.status(500).json({ error: 'Internal Server Error' });
     }
 })
-
+/**
+ * @swagger
+ * /courses/{courseId}:
+ *   put:
+ *     summary: Actualiza un curso existente.
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         required: true
+ *         description: ID del curso a actualizar.
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       description: Nuevos datos del curso.
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/definitions/CourseFormInputs'
+ *     responses:
+ *       201:
+ *         description: Curso actualizado exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               name: 'Nuevo Curso'
+ *               description: 'Nueva descripción del curso'
+ *               price: 24.99
+ *               categories: ['NuevaCategoria1', 'NuevaCategoria2']
+ *               language: 'Inglés'
+ *               creator: 'UsuarioActualizado'
+ *               score: 4.2
+ *       404:
+ *         description: Curso no encontrado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.put('/:courseId', async (req: Request, res: Response) => {
   try {
     let decodedToken: IUser = await getPayloadFromToken(
@@ -237,7 +407,27 @@ router.put('/:courseId', async (req: Request, res: Response) => {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 })
-
+/**
+ * @swagger
+ * /courses/{courseId}:
+ *   delete:
+ *     summary: Elimina un curso existente.
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         required: true
+ *         description: ID del curso a eliminar.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Curso eliminado exitosamente.
+ *       404:
+ *         description: Curso no encontrado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.delete('/:courseId', async (req: Request, res: Response) => {
   try {
     let decodedToken: IUser = await getPayloadFromToken(
@@ -300,7 +490,41 @@ router.delete('/:courseId', async (req: Request, res: Response) => {
     return res.status(500).json({ error: 'Internal Server Error' });
   }
 })
-
+/**
+ * @swagger
+ * /courses/{courseId}:
+ *   get:
+ *     summary: Obtiene la información detallada de un curso.
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         required: true
+ *         description: ID del curso a obtener.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Información del curso obtenida exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               _id: '1234567890'
+ *               name: 'Curso Ejemplo'
+ *               description: 'Descripción del curso.'
+ *               price: 19.99
+ *               categories: ['Categoría1', 'Categoría2']
+ *               language: 'Español'
+ *               creator: 'UsuarioCreador'
+ *               score: 4.5
+ *               access: ['Estudiante1', 'Estudiante2']
+ *               classes: ['Clase1', 'Clase2']
+ *               materials: ['Material1', 'Material2']
+ *       404:
+ *         description: Curso no encontrado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/:courseId', async (req: Request, res: Response) => {
     try {
       let decodedToken: IUser = await getPayloadFromToken(
@@ -348,7 +572,38 @@ router.get('/:courseId', async (req: Request, res: Response) => {
       }
     }
 })
-
+/**
+ * @swagger
+ * /courses/{courseId}/classes:
+ *   get:
+ *     summary: Obtiene las clases de un curso específico.
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         required: true
+ *         description: ID del curso del cual obtener las clases.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Clases obtenidas exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - _id: 'Clase1'
+ *                 title: 'Clase de Introducción'
+ *                 description: 'Descripción de la clase de introducción.'
+ *                 videoUrl: 'https://www.youtube.com/watch?v=1234567890'
+ *               - _id: 'Clase2'
+ *                 title: 'Segunda Clase'
+ *                 description: 'Descripción de la segunda clase.'
+ *                 videoUrl: 'https://www.youtube.com/watch?v=0987654321'
+ *       404:
+ *         description: Curso no encontrado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/:courseId/classes', async (req: Request, res: Response) => {
     try {
       let decodedToken: IUser = await getPayloadFromToken(
@@ -417,7 +672,38 @@ router.get('/:courseId/classes', async (req: Request, res: Response) => {
       }
     }
 })
-
+/**
+ * @swagger
+ * /courses/{courseId}/materials:
+ *   get:
+ *     summary: Obtiene los materiales de un curso específico.
+ *     tags: [Courses]
+ *     parameters:
+ *       - in: path
+ *         name: courseId
+ *         required: true
+ *         description: ID del curso del cual obtener los materiales.
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Materiales obtenidos exitosamente.
+ *         content:
+ *           application/json:
+ *             example:
+ *               - _id: 'Material1'
+ *                 title: 'Material de Lectura'
+ *                 type: 'Documento'
+ *                 url: 'https://www.example.com/material1.pdf'
+ *               - _id: 'Material2'
+ *                 title: 'Video Tutorial'
+ *                 type: 'Video'
+ *                 url: 'https://www.youtube.com/watch?v=abcdefghijk'
+ *       404:
+ *         description: Curso no encontrado.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/:courseId/materials', async (req: Request, res: Response) => {
     try {
       let decodedToken: IUser = await getPayloadFromToken(
